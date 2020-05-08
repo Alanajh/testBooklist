2493518c22b860bfe1d0db98ceebb3ef241563cf
Alana Harmon <alana.harmon@yahoo.com>
2020-05-08 02:56:34 -0500

    Added the routing folder and seperated the routing from the app.js.

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,11 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const postsRoutes = require("./routes/posts");
 
-//execute express as a funtion
-const app = express();
+const router = express();
 
 mongoose.connect("mongodb+srv://Admin:PTk2ek7yZMGXMSxI@cluster0-9nslc.mongodb.net/node-angular?retryWrites=true&w=majority",
   {
@@ -19,72 +18,16 @@ mongoose.connect("mongodb+srv://Admin:PTk2ek7yZMGXMSxI@cluster0-9nslc.mongodb.ne
       console.log('Connection the the database failed.');
     });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
+router.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   next();
 });
 
-//PTk2ek7yZMGXMSxI
+router.use("/api/posts", postsRoutes);
 
-app.post("/api/posts", (req, res, next) => {
-  const posts = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  posts.save().then(createdPost => {
-    console.log(createdPost);
-    res.status(201).json({
-      message: 'Successfully done soldier!',
-      postId: createdPost._id
-  });
- // console.log(posts);
- // posts.save();
-  
-    });
-  });
-
-  app.put("/api/posts/:id", (req, res, next) => {
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content
-    });
-    Post.updateOne({_id: req.params.id}, post).then(result => {
-      res.status(200).json({message: "update successful"});
-    });
-  });
-
-  
-app.get('/api/posts', (req, res, next) => {
-Post.find().then(documents => {
-  res.status(200).json({
-    message: "Posts fetched successfully",
-    posts: documents
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if(post) {
-      res.status(200).json(post);
-    } else {
-       res.status(404).json({message: 'Post not found.'});
-     }
-  })
-})
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id}).then(result => {
-    console.log(result);
-    console.log(req.params.id);
-    res.status(200).json({ message: "Post deleted!" });
-  });
-});
-
-module.exports = app;
+module.exports = router;
