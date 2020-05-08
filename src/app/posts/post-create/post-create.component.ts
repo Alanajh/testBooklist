df53d877937a71945f5309b6ad53a3b08f635fc1
Alana Harmon <alana.harmon@yahoo.com>
2020-05-08 03:32:17 -0500

    Working spinner when waiting for the page to load content.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,7 @@ import { Post } from '../../posts/post.model';
 import { NgForm } from "@angular/forms";
 import { PostsService } from '../post.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-post-create',
@@ -13,6 +14,7 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 export class PostCreateComponent implements OnInit{
   enteredTitle = "";
   enteredContent= "";
+  isLoading = false;
   post: Post;
   private mode = 'create';
   private postId: string;
@@ -25,8 +27,10 @@ export class PostCreateComponent implements OnInit{
       if(paramMap.has('postId')){
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
+          this.isLoading = true;
         this.postsService.getPost(this.postId)
         .subscribe(postData => {
+          this.isLoading = false;
           this.post = {id: postData._id, title: postData.title, content: postData.content};
         });
       }else{
@@ -40,7 +44,7 @@ export class PostCreateComponent implements OnInit{
     if(form.invalid){
       return;
     }
-
+    this.isLoading = true;
     if (this.mode === 'create'){
       this.postsService.addPost(form.value.title, form.value.content);
     } else {
